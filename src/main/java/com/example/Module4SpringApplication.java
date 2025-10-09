1b48931b057f11b6dc05803be6b2a3dd028b306a
GrigoriyVolk <grigoriyvolk228@gmail.com>
2025-10-09 12:31:55 +0300

    first commit

---
+++ src/main/java/com/example/Module4SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4SpringApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(Module4SpringApplication.class, args);
+	}
+
+}
